ea4046a32cf76a5a326d2de5b28ab903c389ead5
Elizabeth Tom <elizabeth_tom@excite.com>
2017-08-22 13:26:25 -0500

    update 8/22/17 13:26

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	var btnReset;
 	var characters = [
 		{
 			'name': 'Obi-Wan Kenobi',
@@ -26,29 +27,31 @@ $(document).ready(function() {
 			'counterAttackPoints': 25
 		}
 	];
-	// var characterIcons = [];
 	var counterAttackPoints;
 	var enemyChosen = false;
 	var enemyIndex;
+	var gameOver = false;
+	var icon;
 	var numDefeated = 0;
 	var numCharacters = 4;
 	var playerAttackPoints;
 	var playerChosen = false;
 	var playerIndex;
-	var icon;
-
+	
 	updateHealthPoints();
 
 	// Character icon click handler
 	$(".character-icon").on("click", function() {
 		$("footer").empty();
+		// If player hasn't been chosen, then character clicked becomes the player
 		if (!playerChosen)
 		{
 			playerChosen = true;
 			playerIndex = $(this).attr("index");
 			icon = $(this).detach();
-			// characterIcons.push(icon);
+			// Move chosen character to 'Your Character' div
 			$(".your-character").append(icon);
+			// For all other remaining characters, change background color and move to 'Enemies Available'
 			$(".character-icon").each(function( index ) 
 			{
   				if (($(this).attr("index")) !== playerIndex)
@@ -59,14 +62,16 @@ $(document).ready(function() {
   				}
 			});
 		}
+		// If player has been chosen and enemy has not been chosen, then character clicked becomes the defender
 		else if (!enemyChosen)
 		{
 			enemyIndex = $(this).attr("index");
+			// Verify that user did not choose same character for both player and enemy
 			if (enemyIndex !== playerIndex)
 			{
+				// Change background color and move to 'Defender'
 				enemyChosen = true;
 				icon = $(this).detach();
-				// characterIcons.push(icon);
 				icon.css({'background-color': 'black', 'border-color': '#0fad1c', 'color': '#FEFFF7'});
 				$(".defender").append(icon);
 			}
@@ -78,22 +83,37 @@ $(document).ready(function() {
 		$("footer").empty();
 		if (this.id === 'btn-attack')
 		{
-			if (playerChosen && enemyChosen)
+			// Attack button should only function when it's not Game Over
+			if (!gameOver)
 			{
-				playerAttackPoints = characters[playerIndex].attackPoints;
-				counterAttackPoints = characters[enemyIndex].counterAttackPoints;
-				characters[enemyIndex].healthPoints -= playerAttackPoints;
-				characters[playerIndex].healthPoints -= counterAttackPoints;
-				updateHealthPoints();
-				$("footer").append("<p>You attacked " + characters[enemyIndex].name + " for " + playerAttackPoints + " damage.</p>");
-				$("footer").append("<p>" + characters[enemyIndex].name + " attacked you back for " + counterAttackPoints + " damage.</p>");
-				characters[playerIndex].attackPoints += characters[playerIndex].counterAttackPoints;
-				checkBattleResults();
+				// If player and enemy are both chosen, then player attacks and enemy counter attacks
+				if (playerChosen && enemyChosen)
+				{
+					playerAttackPoints = characters[playerIndex].attackPoints;
+					counterAttackPoints = characters[enemyIndex].counterAttackPoints;
+					characters[enemyIndex].healthPoints -= playerAttackPoints;
+					characters[playerIndex].healthPoints -= counterAttackPoints;
+					updateHealthPoints();
+					$("footer").append("<p>You attacked " + characters[enemyIndex].name + " for " + playerAttackPoints + " damage.</p>");
+					$("footer").append("<p>" + characters[enemyIndex].name + " attacked you back for " + counterAttackPoints + " damage.</p>");
+					characters[playerIndex].attackPoints += characters[playerIndex].counterAttackPoints;
+					// Check remaining health points to see if either player has been defeated
+					checkBattleResults();
+				}
+				else
+				{
+					$("footer").append("No enemy here");
+				}
 			}
 			else
 			{
-				$("footer").append("No enemy here");
+				setResetButton();
 			}
+		}
+		else if (this.id === 'btn-reset')
+		{
+			// location.reload();
+			alert("reset button was clicked");
 		}
 	});
 
@@ -102,8 +122,10 @@ $(document).ready(function() {
 	{
 		if (characters[playerIndex].healthPoints <= 0)
 		{
+			gameOver = true;
 			$("footer").empty();
-			$("footer").append("You've been defeated...GAME OVER!!!");
+			$("footer").append("<p>You've been defeated...GAME OVER!!!</p>");
+			setResetButton();
 		}
 		else if (characters[enemyIndex].healthPoints <= 0)
 		{
@@ -111,15 +133,29 @@ $(document).ready(function() {
 			$("footer").empty();
 			if (numDefeated < numCharacters - 1)
 			{
-				$("footer").append("You have defeated " + characters[enemyIndex].name + ", you can choose to fight another enemy.");
+				$("footer").append("<p>You have defeated " + characters[enemyIndex].name + ", you can choose to fight another enemy.</p>");
 				enemyChosen = false;
 			}
 			else
 			{
-				$("footer").append("You won!!!! GAME OVER!!!");	
+				gameOver = true;
+				$("footer").append("<p>You won!!!! GAME OVER!!!</p>");	
+				setResetButton();
 			}
 			icon.remove();
 		}
+	}
+
+	// Create reset button and place on page
+	function setResetButton() 
+	{
+		btnReset = $("<button id='btn-reset'>Reset</button>");
+	    $("footer").append(btnReset);
+	    btnReset.on("click", function() {
+	    	location.reload();
+	    });
+	    console.log($("#btn-attack"));
+	    console.log(btnReset);
 	}
 
 	// Update the value displayed on each character icon to reflect the character's current number of health points
